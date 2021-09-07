d426fc2a7a23a67195556a9959f26550cbb5fa00
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-09-07 23:46:25 +0530

    class implementation of DannJS

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 
 
-const input = [[1 , 2, 3, 2.5],
-         [0.5 , -1.1, 3.3, -4.5],
-         [-2.2 , 4, -4.6, 5.5],]
+const nn = new simpleML.Network(2, 2);
 
-const target = [1, 2 , 2];
+nn.log({details:true});
+// network.createLayer('layer0', 4, 3); //name , inputs, neurons
+// network.createLayer('layer1', 3, 3);
+// network.connect(['layer0', 'relu', 'layer1', 'softmax']);
 
-const network = new simpleML();
-network.createLayer('layer0', 4, 3); //name , inputs, neurons
-network.createLayer('layer1', 3, 3);
-network.connect(['layer0', 'relu', 'layer1', 'softmax']);
+// output = network.feedForward(input );
 
-output = network.feedForward(input );
+// network.train(input, target);
 
-network.train(input, target);
+// acc    = network.runBatch_accuracy(output, target);
+// loss   = network.runBatch_loss(output, target);
 
-acc    = network.runBatch_accuracy(output, target);
-loss   = network.runBatch_loss(output, target);
-
-console.log(acc);
+// console.log(acc);
 
 
 
