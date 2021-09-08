65cc436d59ac4ec9745b090f5f78e0e3ab514728
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-09-09 00:52:08 +0530

    example update

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 
 
-const nn = new simpleML();
-
-
-var engine = new simpleML(); //create simpleML engine
-
 const data = [...Array(10000).keys()].map(x => {
- const input =  parseInt(String(Math.random() * 50 - 25),10);
-  return  {input : [input]  , output : [input > 0 ? true : false]}
-});
+    const input =  parseInt(String(Math.random() * 50 - 25),10);
+     return  {input : [input]  , output : [input > 0 ? true : false]}
+   });
 
+//create simpleML engine
+const engine = new simpleML(); 
+
+
+//create a model network
 engine.createNetwork('network1',{
     input    : 1,
     hidden   : [[2, 'reLU']],
@@ -17,11 +17,14 @@ engine.createNetwork('network1',{
     activate : 'sigmoid',
 }); 
 
+
+//train it
 engine.train('network1', data , { 
     lr       : 0.0001,
     loss     : 'bce',
 }); 
 
+//predict the output
 output = engine.predict('network1',[-5]);
 
 console.log(output);
